7176fe23f0365ec5ce3bb3fb8c6a899cd691a197
Edward Misback <elmisback@gmail.com>
2021-09-02 14:46:17 +0000

    First commit.

---
+++ server.js
@@ -0,0 +1,50 @@
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+import { WebSocketServer } from 'ws';
+
+const server = createServer({
+  cert: readFileSync('/etc/letsencrypt/live/auth-rtc.strcat.xyz/fullchain.pem'),
+  key: readFileSync('/etc/letsencrypt/live/auth-rtc.strcat.xyz/privkey.pem')
+});
+
+const wss1 = new WebSocketServer({ noServer: true });
+const wss2 = new WebSocketServer({ noServer: true });
+
+wss1.on('open', () => {})
+
+wss1.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	      });
+	    
+	      ws.send('something');
+});
+
+wss2.on('open', () => {})
+
+wss2.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	      });
+	    
+	      ws.send('something');
+});
+
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = parse(request.url);
+
+  if (pathname === '/host') {
+    wss1.handleUpgrade(request, socket, head, function done(ws) {
+      wss1.emit('connection', ws, request);
+    });
+  } else if (pathname === '/transient') {
+    wss2.handleUpgrade(request, socket, head, function done(ws) {
+      wss2.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(8443);
